75345c93b96a22b6a88dd92f293a9fd0269d436f
Jonathan Guaynora <jguaynora@assanet.com>
2024-11-01 09:39:59 -0500

    added some plugin to nvim
    Plugins:
    - makdown preview
    - oil
    - supermaven
    
    added theme to nvim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,30 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      flavour = "mocha",
+    },
+    integrations = {
+      cmp = true,
+      gitsigns = true,
+      nvimtree = true,
+      treesitter = true,
+      notify = false,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+    },
+  },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
